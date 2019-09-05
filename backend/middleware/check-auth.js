50624357fe38c0336af1513aecff88e23bbceae7
Giovanni Toschi <giovannitosc@gmail.com>
2019-09-05 18:26:39 +0200

    Up to the beginning of lecture 106 // but with errors, recheck from a few lessons back

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -6,6 +6,7 @@ module.exports = (req, res, next) => {
   jwt.verify(token, "secret_this_should_be_longer")
   next();
   } catch (error) {
+    console.log(error)
     res.status(401).json({ message: "Auth failed!"});
   }
 };
